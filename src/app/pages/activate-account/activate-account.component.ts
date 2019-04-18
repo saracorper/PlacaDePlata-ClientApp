11a8db3766ff74afa3d4e42deb8aed50c6c8c0c4
Sara <cordalpernassl@gmail.com>
2019-04-18 11:08:38 +0200

    añadido componente de activacion de cuenta

---
+++ src/app/pages/activate-account/activate-account.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pdp-activate-account',
+  templateUrl: './activate-account.component.html',
+  styleUrls: ['./activate-account.component.scss']
+})
+export class ActivateAccountComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
