3dc9afb1028d9c96be47da5833436b858fd7c931
Sara <cordalpernassl@gmail.com>
2019-04-18 10:38:14 +0200

    implementada funcionalidad de registro

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'pdp-register',
@@ -11,13 +12,20 @@ export class RegisterComponent implements OnInit {
   public password: string = '';
   public confirmPassword: string = '';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
   }
 
   public register():void {
     console.log('register', this.email, this.password, this.confirmPassword);
+    
+    const body = {
+      password: this.password,
+      email: this.email
+    };
+
+    this.http.post("http://localhost:3000/api/users", body).subscribe();
   }
 
 }
