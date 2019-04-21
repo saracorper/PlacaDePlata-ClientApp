590824dcd56aed6a1ccb3a542121e00c516ff0e8
Sara <cordalpernassl@gmail.com>
2019-04-21 20:28:11 +0200

    añadida refactorizacion del servicio login e implementado el servicios toasty para gestion de errores

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ActivateAccountComponent } from './pages/activate-account/activate-account.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
+import { ToastyModule } from 'ng2-toasty';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { GalleryComponent } from './pages/gallery/gallery.component';
     AppRoutingModule,
     HttpClientModule,
     StorageServiceModule,
-    FormsModule 
+    FormsModule,
+    ToastyModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { LocalStorageService } from '../../services/local-storage.service';
 import { LoginService } from 'src/app/services/login.service';
 import { Router } from '@angular/router';
+import { ToastyService, ToastyConfig } from 'ng2-toasty';
 
 @Component({
   selector: 'pdp-login',
@@ -14,7 +15,14 @@ export class LoginComponent implements OnInit {
   public valEmail: string = '';
   public valPass: string = '';
 
-  constructor(private loginService: LoginService, private storage: LocalStorageService, private router: Router) { }
+  constructor(private loginService: LoginService,
+     private storage: LocalStorageService,
+     private router: Router,
+     private toastyService: ToastyService,
+     private toastyConfig: ToastyConfig
+    ) { 
+      this.toastyConfig.theme = "bootstrap";
+    }
 
 
   ngOnInit() {
@@ -24,16 +32,31 @@ export class LoginComponent implements OnInit {
    * login
    */
   public login():void {
-    console.log('user', this.valEmail);
-    console.log('password', this.valPass);
+
     const body = {
       password: this.valPass,
       email: this.valEmail
     }
-    this.loginService.log(body).subscribe(res => {
-      this.storage.save('token', res.token)
-      this.router.navigateByUrl('gallery');
-    })
-  }
 
+    this.loginService.log(body).subscribe(res => {
+      this.storage.save('token', res.token);
+      this.router.navigateByUrl('gallery');
+    },
+    error => {
+      let config = {
+        title: 'Error.',
+        msg: 'Credenciales incorrectas.',
+        showClose: true,
+        timeout: 3000,
+        theme: 'bootstrap'
+      };
+      if(error.status == 404 || error.status == 400)
+        this.toastyService.error(config);
+      else{
+        config.msg = "Se ha producido un error inesperado";
+        this.toastyService.error(config);
+      }
+      
+    });
+  }
 }
