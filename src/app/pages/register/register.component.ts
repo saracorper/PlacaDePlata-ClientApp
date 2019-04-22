08b005192c156d0bfb5240cf0bd3ff5afa568a9a
Sara <cordalpernassl@gmail.com>
2019-04-22 22:37:31 +0200

    aplicado ng2 toasty a el registro de usuario

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,49 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { ToastyService, ToastyConfig } from 'ng2-toasty';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Router } from "@angular/router";
+import { ToastyService, ToastyConfig } from "ng2-toasty";
 @Component({
-  selector: 'pdp-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  selector: "pdp-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"]
 })
 export class RegisterComponent implements OnInit {
+  public emailVal: string = "";
+  public passwordVal: string = "";
+  public confPasswordVal: string = "";
 
-  public emailVal: string = '';
-  public passwordVal: string = '';
-  public confPasswordVal: string = '';
-
-  constructor(private http: HttpClient, 
+  constructor(
+    private http: HttpClient,
     private router: Router,
     private toastyService: ToastyService,
-    private toastyConfig: ToastyConfig) { }
+    private toastyConfig: ToastyConfig
+  ) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  public register():void {
-    
+  public register(): void {
     const body = {
-        password: this.confPasswordVal,
-        email: this.emailVal
+      password: this.confPasswordVal,
+      email: this.emailVal
     };
 
-    this.http.post("http://localhost:3000/api/users", body).subscribe(res => 
-    error =>{
-      let config = {
-        title: 'Error.',
-        msg: 'Email ya registrado.',
-        showClose: true,
-        timeout: 3000,
-        theme: 'bootstrap'
-      };
-      if (error.status == 404 || error.status == 400)
-        this.toastyService.error(config);
-      else {
-        config.msg = "Se ha producido un error inesperado";
-        this.toastyService.error(config);
+    let configSuccess = {
+      title: "Registro",
+      msg: "Usuario registrado con éxito",
+      showClose: true,
+      timeout: 3000,
+      theme: "bootstrap"
+    };
+    
+    this.http.post("http://localhost:3000/api/users", body).subscribe(
+      res => {
+        this.toastyService.success(configSuccess);
+      },
+      error => {
+        let configError = {
+          title: "Error.",
+          msg: "Email ya registrado.",
+          showClose: true,
+          timeout: 3000,
+          theme: "bootstrap"
+        };
+
+        if (error.status == 400) this.toastyService.error(configError);
+        else {
+          configError.msg = "Se ha producido un error inesperado";
+          this.toastyService.error(configError);
+        }
       }
-    },
     );
   }
 }
