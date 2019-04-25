68b12c40ab41388814daa9077ad11fd111464032
Sara <cordalpernassl@gmail.com>
2019-04-25 22:25:10 +0200

    añadido viewDatail  de galeria de post

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from 'src/app/services/post.service';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'pdp-gallery',
@@ -14,19 +15,19 @@ export class GalleryComponent implements OnInit {
   public posts = [];
     
 
-  constructor(private postService: PostService, private localStorage: LocalStorageService) { }
+  constructor(private postService: PostService, private localStorage: LocalStorageService, private router: Router) { }
 
   ngOnInit() {
 
     let token = this.localStorage.read('token') as string;
-    console.log('token leido :', token);
-
-    const helper = new JwtHelperService();
-    const decoded = helper.decodeToken(token);
 
     this.postService.list(token).subscribe(posts =>{
-      console.log(posts);
       this.posts = posts;
     });
+  }
+
+  public viewDetail(postId: string, userId: string): void {
+
+    this.router.navigateByUrl(`users/${userId}/posts/${postId}`);
   }
 }
