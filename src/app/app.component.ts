05610c10d3d6fb56b66a2f920a188935fe27327a
Sara <cordalpernassl@gmail.com>
2019-04-26 18:52:09 +0200

    añadidos link de paginas al menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
+import { LocalStorageService } from './services/local-storage.service';
 
 @Component({
   selector: 'pdp-root',
   templateUrl: './app.component.html' ,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'placa-de-plata';
+export class AppComponent implements OnInit{
+
+  public user: { 
+    _id: string,
+    fullName: string
+  };
+
+  constructor(private userService: UserService, private storage: LocalStorageService) {}
+
+  
+  
+  ngOnInit(): void {
+    
+    // this.userService.get()
+  }
 }
 
